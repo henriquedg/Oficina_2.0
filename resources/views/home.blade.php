94bf804bc8a46e9f49af2bc4b343a805f7f15df1
Henrique Duarte Guerra <henrihevelin@gmail.com>
2020-08-06 19:27:54 -0300

    3.2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,20 +8,91 @@
                 <div class="card-header">Orçamentos cadastrados</div>
 
                 <div class="card-body">
-                    @foreach($users as $user)
-                        <div class="row pb-5 pl-3">
 
-                            ID: {{$user->id}}<br>
-                            Cliente: {{$user->cliente}}<br>
-                            Data: {{$user->data}}<br>
-                            Hora: {{$user->hora}}<br>
-                            Vendedor: {{$user->vendedor}}<br>
-                            Descrição: {{$user->descricao}}<br>
-                            Valor orçado: R$ {{$user->valor}}<br>
+                    <div class="pb-5">
+                        <form action="/search" method="POST" role="search">
+                            {{ csrf_field() }}
+                            Selecione uma opção para filtrar os resultados: 
+                            <br><br><input type="checkbox" name="cliente" value="cliente"> Cliente<br>
+                            <input type="checkbox" name="vendedor" value="vendedor"> Vendedor<br><br>
+
+                            <div class="input-group">
+                                <input type="text" class="form-control" name="q"
+                                    placeholder="Pesquisar nome"> <span class="input-group-btn">
+
+                                    <button type="submit" class="btn btn-default">
+                                        <span class="glyphicon glyphicon-search"></span>
+                                        Pesquisar
+                                    </button>
+
+                                </span>
+                            </div>
                             
+                            
+                        </form>
+
+                        <br>
+                        <br>
+                        <div class="row pb-5 pl-3">
+                            <form action="/search" method="POST" role="search">
+                                {{ csrf_field() }}
+                                Pesquisa por data:<br><br>
+                                <div class="input-group">
+                                    De: &nbsp;
+                                    <input type="date" class="form-control" name="data1">
+                                        &nbsp; até: &nbsp;
+                                        <input type="date" class="form-control" name="data2"> 
+
+                                            <button type="submit" class="btn btn-default">
+                                                <span class="glyphicon glyphicon-search"></span>
+                                                Pesquisar
+                                            </button>
+
+                                        
+                                    
+                                </div>
+                                
+                                
+                            </form>
                         </div>
-                        
-                    @endforeach
+                    </div>
+
+
+                    <div class="container">
+                        @if(isset($details))
+                            <p> Resultados encontrados: <b> {{ $query }} </b></p>
+                        <br>
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Cliente</th>
+                                    <th>Data</th>
+                                    <th>Hora</th>
+                                    <th>Vendedor</th>
+                                    <th>Descrição</th>
+                                    <th>Valor orçado</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($details as $user)
+                                <tr>
+                                    <td>{{$user->id}}</td>
+                                    <td>{{$user->cliente}}</td>
+                                    <td>{{$user->data}}</td>
+                                    <td>{{$user->hora}}</td>
+                                    <td>{{$user->vendedor}}</td>
+                                    <td>{{$user->descricao}}</td>
+                                    <td>R$ {{$user->valor}}</td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                        @endif
+                    </div>
+
+
+                   
 
                     
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,34 @@
 |
 */
 
+use App\User;
+use Illuminate\Support\Facades\Input;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::any('/search',function(){
+    $q = Input::get ( 'q' );
+    $opcaoCliente = Input::get ( 'cliente' );
+    $opcaoVendedor = Input::get ( 'vendedor' );
+    $data1 = Input::get ( 'data1' );
+    $data2 = Input::get ( 'data2' );
+
+
+    $user = User::where($opcaoCliente,'LIKE','%'.$q.'%')->orWhere($opcaoVendedor,'LIKE','%'.$q.'%')->orderBy('data','desc')->get();
+
+    $user1 = User::whereBetween('data',[$data1,$data2])->orderBy('data','desc')->get();
+
+    if(count($user) > 0 && $q!=null){
+        return view('home')->withDetails($user)->withQuery ( $q );
+    }
+    else
+
+    if(count($user) > 0 && $data1!=null && $data2!=null){
+        return view('home')->withDetails($user1)->withQuery ( '' );
+    }
+    else return view ('home')->withMessage('Nenhum resultado');
 });
 
 Auth::routes();
